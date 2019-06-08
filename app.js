8ea8af5fa7e7d4859a6d1cd26370c2208b0d8046
Shehryar Naeem <shehryar.naeem44@gmail.com>
2019-06-08 19:06:58 +0500

    Bugs Resolved

--- app.js
+++ app.js
@@ -12,4 +12,4 @@ const extractionRouter=require('./routes/extraction').extractionRouter;
 app.use('/extract',extractionRouter)
 
 const port=process.env.port
-const server = app.listen(port,require('./routes/extraction').train)
+const server = app.listen(process.env.PORT || 5000,require('./routes/extraction').train)
